a6c11f97bb5e5f5102e7b63bcd4f4fbfb0a085ef
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-02-12 06:53:19 +0100

    Enable App Router in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
+  // Enables the App Router (already default in Next 14)
+  experimental: {
+    appDir: true
+  }
+};
+
+module.exports = nextConfig;
