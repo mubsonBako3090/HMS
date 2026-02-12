3fc32cd55327e0ceec1765e28a5eab70da4822b0
Adam Mubarak Bako <mubarakadambako@gmail.com>
2026-02-12 06:41:40 +0100

    Add layout component and global styles import

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,1 +1,16 @@
-u
+import './globals.css';
+
+export const metadata = {
+  title: 'Hospital Management System',
+  description: 'Final Year Project HMS',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
